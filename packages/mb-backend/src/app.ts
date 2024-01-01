33aef23e0fa3398a11152282d55530d631bc0e32
Rodrigo Nogueira <rdg6design@gmail.com>
2024-01-01 17:10:07 -0300

    feat: add CORS middleware configuration

--- packages/mb-backend/src/app.ts
+++ packages/mb-backend/src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 const port = 3001;
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
