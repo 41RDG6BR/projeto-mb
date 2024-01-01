7702f608e0890650ff78bef044dd1aecac1786b4
Rodrigo Nogueira <rdg6design@gmail.com>
2024-01-01 19:40:41 -0300

    feat: add CustomButton component

---
+++ packages/mb-frontend/src/components/CustomButton.tsx
@@ -0,0 +1,27 @@
+// CustomButton.tsx
+import React from 'react';
+
+interface CustomButtonProps {
+  children: React.ReactNode;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  className?: string;
+  style?: React.CSSProperties & {
+    '@media (max-width: 768px)'?: React.CSSProperties;
+  };
+  disabled?: boolean;
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({ children, onClick, className, style, disabled }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={className}
+      style={style}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default CustomButton;
