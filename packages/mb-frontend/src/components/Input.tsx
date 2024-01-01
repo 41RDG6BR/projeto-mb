0890c9defce3894e26deb00b00f9bd8fb96a8f5a
Rodrigo Nogueira <rdg6design@gmail.com>
2024-01-01 19:41:33 -0300

    feat: add Input component

---
+++ packages/mb-frontend/src/components/Input.tsx
@@ -0,0 +1,24 @@
+// Input.tsx
+import React from 'react';
+import '../styles/Input.css';
+
+interface InputProps {
+  type: string;
+  value: string | number;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+}
+
+const Input: React.FC<InputProps> = ({ type, value, onChange, placeholder }) => {
+  return (
+    <input
+      type={type}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      className="custom-input"
+    />
+  );
+};
+
+export default Input;
