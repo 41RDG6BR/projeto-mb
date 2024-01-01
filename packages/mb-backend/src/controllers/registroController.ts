0743dbd490475653a90bfc739e1e0ab89432885f
Rodrigo Nogueira <rdg6design@gmail.com>
2024-01-01 17:39:09 -0300

    feat(adicionarRegistro): add adicionarRegistro function

--- packages/mb-backend/src/controllers/registroController.ts
+++ packages/mb-backend/src/controllers/registroController.ts
@@ -3,3 +3,27 @@ export const getRegistros = (req: Request, res: Response): void => {
   res.json(registers);
 };
 
+export const adicionarRegistro = (req: Request, res: Response): void => {
+  const { disciplina, bimester, nota } = req.body;
+  const chave = `${disciplina}-${bimester}`;
+
+  if (!validateNote(nota)) {
+    res.status(400).json({ mensagem: "A nota deve estar entre 0 e 10." });
+    return;
+  }
+
+  if (registrosMap.has(chave)) {
+    res.status(400).json({ mensagem: "Já existe uma disciplina registrada para este bimestre." });
+  } else {
+    const novoRegistro: Register = {
+      id: Date.now().toString(),
+      bimester,
+      disciplina,
+      nota,
+      criadoEm: new Date(),
+      atualizadoEm: new Date(),
+    };
+    registrosMap.set(chave, novoRegistro);
+    res.json(novoRegistro);
+  }
+};
