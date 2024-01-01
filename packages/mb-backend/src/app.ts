3203c1bc43ca6d4bc8c33aacd6c4abc243f0e4c9
Rodrigo Nogueira <rdg6design@gmail.com>
2024-01-01 17:28:27 -0300

    feat: add route configurations for registers

--- packages/mb-backend/src/app.ts
+++ packages/mb-backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { getRegistros, adicionarRegistro, removerRegistro } from "./controllers/registroController";
 import swaggerUI from "swagger-ui-express";
 import swaggerDocument from "./swagger.json";
 
@@ -9,6 +10,10 @@ const port = 3001;
 app.use(express.json());
 app.use(cors());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+app.get("/registros", getRegistros);
+app.post("/registros", adicionarRegistro);
+app.delete("/registros/:id", removerRegistro);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
