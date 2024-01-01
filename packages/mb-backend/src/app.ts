2a9819ebde499e4c7528bc26255eff6d3518c785
Rodrigo Nogueira <rdg6design@gmail.com>
2024-01-01 17:26:48 -0300

    feat: integrate swagger-ui-express for API documentation

--- packages/mb-backend/src/app.ts
+++ packages/mb-backend/src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
+import swaggerUI from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
+
 const app = express();
 const port = 3001;
 
 app.use(express.json());
 app.use(cors());
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
